9916435aed284b86acb0b264569b9a6506a6f9f7
Vishvesh Naik Ajmeera <ajmeeranaik11@gmail.com>
2024-09-01 02:42:05 +0530

    first commit

---
+++ app/src/main/java/com/arditakrasniqi/healthclinic/presentation/navigation/AppScreens.kt
@@ -0,0 +1,16 @@
+package com.arditakrasniqi.healthclinic.presentation.navigation
+
+sealed class AppScreens(val route: String) {
+
+    object LoginScreen : AppScreens("login")
+    object RegisterScreen : AppScreens("register")
+    object HomeScreen : AppScreens("home")
+    object AcademiaScreen : AppScreens("academia")
+    object SettingsScreen : AppScreens("More")
+    object DoctorsScreen : AppScreens("Doctors")
+    object ClinicHomeScreen : AppScreens("clinic_home")
+    object DoctorsHomeScreen : AppScreens("doctors_home")
+    object PatientAppointmentsScreen: AppScreens("Appointments")
+    object PatientMainScreen: AppScreens("Home")
+    object PatientDetailsScreen: AppScreens("Details")
+}
